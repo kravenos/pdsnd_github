470e2d51c6008270d6f322bb9ab4e9f29dcfe10a
Corvin <corvinbrady@gmail.com>
2021-11-12 17:55:23 +0200

    Modified bikeshare.py get_filters() comments

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -14,7 +14,7 @@ def get_filters():
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
-        Remember to check output carefully
+        Remember to check output carefully again
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
